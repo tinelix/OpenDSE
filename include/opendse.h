543e35355655e8111b816f62c2eea9be4250c50b
Dmitry Tretyakov <tinelix@mail.ru>
2026-05-07 16:00:43 +0700

    Update source

--- include/opendse.h
+++ include/opendse.h
@@ -1,16 +1,23 @@
 #ifndef _OPENDSE_OPENDSE_H
 #define _OPENDSE_OPENDSE_H
 
+#include <utils/c_exts.h>
+#include <mmio/mmio.h>
+
+#ifdef OPENDSE_LIB
+	extern DSE_MMIO* stdmmio;	
+#else
+	#ifdef UNIX
+		DSE_MMIO* stdmmio;
+	#elif WIN32
+		__declspec(dllimport) DSE_MMIO* stdmmio;
+	#endif
+#endif
+
 typedef struct _dse_version {
 	int major;
 	int minor;
 	int patch;
 } DSEVERSION;
-
-/*#ifdef CROCON_LIB
-
-#else
-
-#endif*/
 
 #endif
