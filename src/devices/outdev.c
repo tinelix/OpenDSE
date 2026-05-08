dd92524b9f2b54a6f982671f949f20997297a74e
Dmitry Tretyakov <tinelix@mail.ru>
2026-05-08 08:13:41 +0700

    Add RIFF metadata parsing

--- src/devices/outdev.c
+++ src/devices/outdev.c
@@ -7,7 +7,7 @@ int dse_open_outdev(DSE_OUTDEV* outdev) {
 }
 
 int dse_write_outdev(unsigned char* buffer, int size) {
-	return _dse_pcm_out(buffer, size);		
+	return _dse_pcm_out(buffer, size);
 }
 
 int dse_close_outdev(DSE_OUTDEV* outdev) {
